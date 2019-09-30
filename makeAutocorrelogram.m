ea01cb54ddd44d5da3ae285153cfd79b5e079c36
acumpelik <acumpelik@gmail.com>
2019-09-30 16:49:43 -0400

    acg cont

--- makeAutocorrelogram.m
+++ makeAutocorrelogram.m
@@ -39,7 +39,52 @@ x = [1 0 1 0 1];
 [f, flags] = xcorr(x);
 stem(flags, f)
 
+%% based on binspikes.m
+trialStart = handles.start(6);
+trialEnd = handles.end(6);
 
+% numTrials = length(handles.start); % number of trials
+% dt = 0.001; % bin size in s
+% tmin = trialStart - trialStart;
+% tmax = trialEnd - trialStart;
+% xvec = tmin:dt:tmax;
+% numx = numel(xvec);
+% spikes_binned = zeros(numTrials,numx);
+wndw = 1;
+tmin = -1;
+tmax = 4;
 
+nt = length(handles.start);
+xvec = tmin:dt:tmax;
+nx = numel(xvec);
+spikes_binned = zeros(nt,nx);
+alignHandle = handles.start;
+
+for trial = 1:nt
+    if isfield(handles, 'end')
+        these = spiketimes>=handles.start(trial)-wndw & spiketimes<=handles.end(trial)+wndw;
+        % does wndw have to be 1 second or the timestamp of the window?
+
+        spikesTrial = spiketimes(these) - alignHandle(trial);
+%         spikes_binned(trial,:) = spikesTrial;
+    else
+        these = find(spiketimes>=handles.start(trial)-wndw_l & spiketimes<=handles.leavecpoke(trial)+wndw_r);
+        spikesTrial = spikestimes(these)-alignHandle(trial);
+%         spikes_binned(trial,:) = spikesTrial;
+    end
+    
+    %sort into bins, aligned to a specific event
+    binedges = [xvec,xvec(end)+dt];
+    yind = discretize(spikesTrial,binedges);
+    
+    for m = 1:nx
+        spikes_binned(trial,m) = sum(numel(spikesTrial(yind==m)));
+    end
+    
+end
+%%
+
+cross = xcorr(spikes_binned(:,6));
+stem(cross)
 
 
