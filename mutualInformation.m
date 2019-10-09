1065671d2124429945ff4c8b8f30dc8477fea5ae
acumpelik <acumpelik@gmail.com>
2019-10-09 16:15:56 -0400

    Update mutualInformation.m

--- mutualInformation.m
+++ mutualInformation.m
@@ -1,9 +1,10 @@
 %% load session and find trials where each reward volume was presented
 session = 741;
 loadSession
+numTrials = length(hmat_start);
 
 leftVol = S.pd{1, 1}.this_left_volume;
-unique(leftVol)
+% unique(leftVol)
 
 trialsVol6 =  find(leftVol==6);
 trialsVol12 = find(leftVol==12);
@@ -13,11 +14,12 @@ trialsVol48 = find(leftVol==48);
 %% find spike probability given reward volume P(X|Y)
 % bin spikes in a way that each bin has only 0 or 1 spikes
 
-dt = 0.001;                                             % bin size in s
+dt = 0.0005;                                             % bin size in s
+wndw = 1;
 
 for ii = 1:numTrials
-    startTime = handles.lbups{1}(1);
-    endTime = handles.lbups{1}(end);
+    startTime = handles.lbups{ii}(1);
+    endTime = handles.lbups{ii}(end);
     these = find(spiketimes>=startTime & spiketimes<=endTime);
     spikesStim = spiketimes(these);
 
@@ -32,4 +34,27 @@ for ii = 1:numTrials
 end
 
 % % verify that bins only have 0 or 1 spikes
-% unique(spikesBinned);
+% unique(spikesBinned)
+
+numSpikesPerTrial = sum(spikesBinned, 2);
+
+%% find only trials where the animal was rewarded 24 uL
+[~, chosenval, ~, ~, ~] = parse_choices(S);
+rewardMask24 = chosenval==24;
+spikesBinned(rewardMask24);
+spikeTrains24 = spikesBinned(rewardMask24, :);
+
+% test: count spikes in each trial
+sum(spikeTrains24, 2)
+
+
+% run heatmap, hmat_start, generate PSTH for sessions of a certain volume
+figure;
+plot(xvec, nanmean(hmat(prev_hit==1 & ~isnan(hits),:)), 'b'); hold on 
+plot(xvec, nanmean(hmat(prev_hit==0 & ~isnan(hits),:)), 'k');
+set(gca, 'TickDir', 'out'); box off;
+xlabel('Time from cpoke (s)');
+
+
+
+
