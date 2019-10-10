2094c34470add59817a814ce87c71100ce9d13de
acumpelik <acumpelik@gmail.com>
2019-10-10 10:00:40 -0400

    Update mutualInformation.m

--- mutualInformation.m
+++ mutualInformation.m
@@ -27,7 +27,7 @@ for ii = 1:numTrials
     xvec = startTime:dt:endTime;                        % bins
     numBins = numel(xvec);                              % number of bins
     binEdges = [xvec,xvec(end)+dt];                     % define the bin edges
-    yind = discretize(spikesStim,binEdges);            % find indices of spikes
+    yind = discretize(spikesStim,binEdges);             % find indices of spikes
     for m = 1:numBins
         spikesBinned(ii,m) = sum(numel(spikesStim(yind==m)));
     end
@@ -38,23 +38,56 @@ end
 
 numSpikesPerTrial = sum(spikesBinned, 2);
 
-%% find only trials where the animal was rewarded 24 uL
+%% find only trials where the animal was rewarded n uL
 [~, chosenval, ~, ~, ~] = parse_choices(S);
-rewardMask24 = chosenval==24;
-spikesBinned(rewardMask24);
-spikeTrains24 = spikesBinned(rewardMask24, :);
 
+% 6 uL
+vol = 6;
+rewardMask6 = chosenval==vol;
+spikesBinned(rewardMask6);
+spikeTrains6 = spikesBinned(rewardMask6, :);
 % test: count spikes in each trial
-sum(spikeTrains24, 2)
+sum(spikeTrains6, 2)
 
+% % 12 uL
+% vol = 12;
+% rewardMask12 = chosenval==vol;
+% spikesBinned(rewardMask12);
+% spikeTrains12 = spikesBinned(rewardMask12, :);
+% % test: count spikes in each trial
+% sum(spikeTrains12, 2)
+
+% % 24 uL
+% vol = 24;
+% rewardMask24 = chosenval==vol;
+% spikesBinned(rewardMask24);
+% spikeTrains24 = spikesBinned(rewardMask24, :);
+% % test: count spikes in each trial
+% sum(spikeTrains24, 2)
+
+% % 48 uL
+% vol = 48;
+% rewardMask48 = chosenval==vol;
+% spikesBinned(rewardMask48);
+% spikeTrains48 = spikesBinned(rewardMask48, :);
+% % test: count spikes in each trial
+% sum(spikeTrains48, 2)
+
+hmat = hmat_start; % which trial component we're aligning to
 
 % run heatmap, hmat_start, generate PSTH for sessions of a certain volume
 figure;
-plot(xvec, nanmean(hmat(prev_hit==1 & ~isnan(hits),:)), 'b'); hold on 
-plot(xvec, nanmean(hmat(prev_hit==0 & ~isnan(hits),:)), 'k');
-set(gca, 'TickDir', 'out'); box off;
-xlabel('Time from cpoke (s)');
+imagesc(spikeTrains6); % imagesc = image with scaled colors
+title(strcat(['Raster plot for cell #', num2str(session), '. Trials with reward volume = 6 uL']));
+set(gca, 'TickDir', 'out'); box off; colorbar % gca: current axes for chart; set tick direction to be outside
 
+%% 
+date = char(datetime('now', 'Format', 'MMddyyyy_HHmmss'));
+                                            % create a timestamp so Matlab doesn't overwrite figures
+filename = strcat(['Heatmap_session', num2str(session), '_rewardvolume6_', date]);
+                                            % concatenate file name
+savefig(filename)                           % save as .fig
 
+saveas(gcf, filename, 'jpeg')               % save as .jpg
 
 
