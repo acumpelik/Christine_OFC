a0e231358e07bb347b1691f3f8f02c84dc76e486
acumpelik <acumpelik@gmail.com>
2019-10-11 10:57:42 -0400

    updated mutual information, added David's example plot of PSTHs by reward volume

--- mutualInformation.m
+++ mutualInformation.m
@@ -6,16 +6,19 @@ numTrials = length(hmat_start);
 leftVol = S.pd{1, 1}.this_left_volume;
 % unique(leftVol)
 
-trialsVol6 =  find(leftVol==6);
-trialsVol12 = find(leftVol==12);
-trialsVol24 = find(leftVol==24);
-trialsVol48 = find(leftVol==48);
-
 %% find spike probability given reward volume P(X|Y)
 % bin spikes in a way that each bin has only 0 or 1 spikes
 
-dt = 0.0005;                                             % bin size in s
-wndw = 1;
+% find longest stimOn period:
+startTimes = cellfun(@(x) x(1), handles.lbups);
+endTimes = cellfun(@(x) x(end), handles.lbups);
+durs = endTimes - startTimes;                           % durations
+[maxTrialDur, maxTrialIndex] = max(durs);               % find duration and index of longest trial
+
+dt = 0.0005;                                            % bin size in s
+xvec = 0:dt:maxTrialDur;                                % bins
+numBins = numel(xvec);                                  % number of bins
+binEdges = [xvec,xvec(end)+dt];                         % define the bin edges
 
 for ii = 1:numTrials
     startTime = handles.lbups{ii}(1);
@@ -23,10 +26,6 @@ for ii = 1:numTrials
     these = find(spiketimes>=startTime & spiketimes<=endTime);
     spikesStim = spiketimes(these);
 
-%     totalTime = endTime - startTime; probbaly don't need
-    xvec = startTime:dt:endTime;                        % bins
-    numBins = numel(xvec);                              % number of bins
-    binEdges = [xvec,xvec(end)+dt];                     % define the bin edges
     yind = discretize(spikesStim,binEdges);             % find indices of spikes
     for m = 1:numBins
         spikesBinned(ii,m) = sum(numel(spikesStim(yind==m)));
