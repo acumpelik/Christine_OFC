30fc4f69a32f457c8597272cbf297a5b1579b2ae
acumpelik <acumpelik@gmail.com>
2019-10-17 11:41:13 -0400

    Update example_plot_for_Andrea.m

--- example_plot_for_Andrea.m
+++ example_plot_for_Andrea.m
@@ -15,15 +15,15 @@ load(strcat([fnames{f_ind},'.mat']))            % load session
 %                                                 % trial start aligned to a time period from -2 to 4
 
 % filter by rewards and certain rew. volume
-Rchosen_mask = ~isnan(chosenval);
-R6_mask = chosenval==6;                         % a logical that finds trials where rat got 6 uL
+RN_mask = ~isnan(chosenval);                        % a logical that finds only rewarded trials
+R6_mask = chosenval==6;                             % a logical that finds trials where rat got 6 uL
 R12_mask = chosenval==12;
 R24_mask = chosenval==24;
 R48_mask = chosenval==48;
 
 figure(1)
 clf
-plot(xvec_start,nanmean(hmat_start(Rchosen_mask,:),1),'k','linewidth',2)
+plot(xvec_start,nanmean(hmat_start(RN_mask,:),1),'k','linewidth',2)
 hold on
 plot(xvec_start,nanmean(hmat_start(R6_mask,:),1),'linewidth',2)
 
@@ -33,6 +33,7 @@ plot(xvec_start,nanmean(hmat_start(R24_mask,:),1),'linewidth',2)
 
 plot(xvec_start,nanmean(hmat_start(R48_mask,:),1),'linewidth',2)
 
+% plot designations
 set(gca,'fontsize',15)
 xlabel('time (s)')
 ylabel('rate (Hz)')
@@ -46,7 +47,7 @@ legend('averaged','6 ul','12','24','48')
 % savefig(filename)                           % save as .fig
 % saveas(gcf, filename, 'jpeg')               % save as .jpg
 
-%% look at all session rewards
+%% plot heatmap of all session rewards
 
 % load a concatenated data set that David made, and you have the code for
 a = load('concatdata_ofc_pokeend.mat');
@@ -54,64 +55,66 @@ A = a.A;
 
 %%
 
+% count all sessions, and define a mask for counting sessions filtered according to my parameters
 numSessions_old = numel(A);                 % number of all sessions
-usableVec = zeros(numel(A),1);         % mask of sessions that are usable (fire more than 2x/trial)
-for j = 1:numSessions_old                   % filter sessions for all the things I want
-    % if you want to do more filtering, add it here. you already code for it
-    % to remove multi unit
+usableVec = zeros(numel(A),1);              % logical of usable sessions (fire more than 2x/trial)
+
+% filter sessions for usability and other the things I want
+for j = 1:numSessions_old                   
+    % can add more filtering here. you already code for it to remove MUA
     if A{j}.isUsable
         usableVec(j) = 1;
     end
 end
-usableSessions = sum(usableVec);            % count usable sessions
+usableSessions = sum(usableVec);            % count number of usable sessions
 
-hmat_usable = zeros(usableSessions,numel(A{1}.xvec)); % define a mask for XXXXXXXXX
+% define a heatmap mask with filtered sessions
+hmat_usable = zeros(usableSessions,numel(A{1}.xvec));
 
 % loop over each session, and PSTH add to hmat;
 usableInds = find(usableVec);               % indices of usable sessions
-nspikesPerSession = zeros(usableSessions,1); % define mask for spikes in session
+avgSpikesPerSession = zeros(usableSessions,1); % define mask for avg # of spikes per session
 for j = 1:usableSessions
     if  A{usableInds(j)}.isUsable           % if session is usable, grab correct index
      hmat_usable(j,:) = nanmean( A{usableInds(j)}.hmat,1 );
     end
     
     % average number (over trials) of spikes for each session
-    % can chnage to firing rate if you want. would need to makes ure time
+    % can change to firing rate if you want. would need to make sure time
     % corresponds to when those spikes counts were taken
-    nspikesPerSession(j) = nanmean(A{usableInds(j)}.nspikes);
+    avgSpikesPerSession(j) = nanmean(A{usableInds(j)}.nspikes);
 end
 
 
 %% order the data
-[B,I] = sort(nspikesPerSession);
-hmat_sess_sorted = hmat_usable(I,:);
+[sessionsByAvgFR,indicesAvgFR] = sort(avgSpikesPerSession);
+hmat_sess_sorted = hmat_usable(indicesAvgFR,:);
 
 % surf(A{1}.xvec,1:usableSessions,hmat_sess_sorted)
 imagesc(A{1}.xvec,1:usableSessions,hmat_sess_sorted)
-colormap hsv
+colormap default
 xlabel('time (s)')
 ylabel('session')
 set(gca,'fontsize',15)
 set(gca, 'TickDir', 'out'); box off; colorbar
 
 %% subset of high-firing data
-imagesc(A{1}.xvec,1:usableSessions,hmat_sess_sorted)
-
 % count only sessions that have B greater than 100
-% variables are: B, hmat_sess_sorted, I
-
-% hmat_high_firing = zeros(sum(B>100), numel(A{1}.xvec);
-
-hmat_high_firing = hmat_sess_sorted(:, I(B>100));
-% imagesc(A{1}.xvec,1:usableSessions,hmat_sess_sorted)
-imagesc(A{1}.xvec,1:sum(B>100),hmat_high_firing)
+% variables are: sessionsByAvgFR, hmat_sess_sorted, indicesAvgFR
 % 
-% for 1:length(B)
-%     if B>100:
-%         hma
-%     end
-% end
+% hmat_high_firing = zeros(sum(sessionsByAvgFR>100), numel(A{1}.xvec));
+% 
+% hmat_high_firing = hmat_sess_sorted(:, indicesAvgFR(sessionsByAvgFR>100));
+% % imagesc(A{1}.xvec,1:usableSessions,hmat_sess_sorted)
+% imagesc(A{1}.xvec,1:sum(sessionsByAvgFR>100),hmat_high_firing)
+% % 
+% % for 1:length(B)
+% %     if sessionsByAvgFR>100:
+% %         hma
+% %     end
+% % end
 
+%% z-scored
 
     
 
