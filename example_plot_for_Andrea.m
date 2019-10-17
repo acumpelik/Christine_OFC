f117fa3081944aca88b059f8e84e793caffa1552
acumpelik <acumpelik@gmail.com>
2019-10-17 14:14:18 -0400

    Update example_plot_for_Andrea.m

--- example_plot_for_Andrea.m
+++ example_plot_for_Andrea.m
@@ -3,7 +3,7 @@
 
 cd('C:\Christine_data')                         % access data folder
 [fnames, units, ~, ~] = getfnames;              % load filenames and info about multiunits
-f_ind = 1168;                                    % choose a session
+f_ind = 741;                                    % choose a session
 
 load(strcat([fnames{f_ind},'.mat']))            % load session
 [~, chosenval, hits, ~] = parse_choices(S);     % chosenval = reward amount rat received
@@ -87,8 +87,8 @@ end
 
 
 %% order the data
-[sessionsByAvgFR,indicesAvgFR] = sort(avgSpikesPerSession);
-hmat_sess_sorted = hmat_usable(indicesAvgFR,:);
+[sessionsByNumSpikes,indicesAvgNumSpikes] = sort(avgSpikesPerSession);
+hmat_sess_sorted = hmat_usable(indicesAvgNumSpikes,:);
 
 % surf(A{1}.xvec,1:usableSessions,hmat_sess_sorted)
 imagesc(A{1}.xvec,1:usableSessions,hmat_sess_sorted)
@@ -101,21 +101,41 @@ set(gca, 'TickDir', 'out'); box off; colorbar
 %% subset of high-firing data
 % count only sessions that have B greater than 100
 % variables are: sessionsByAvgFR, hmat_sess_sorted, indicesAvgFR
+
+hmat_high_firing = zeros(sum(sessionsByNumSpikes>100), numel(A{1}.xvec));
+num_high_firing_sessions = sum(sessionsByNumSpikes>100);
+
+hmat_high_firing = hmat_sess_sorted(indicesAvgNumSpikes(sessionsByNumSpikes>100), :);
+% imagesc(A{1}.xvec,1:usableSessions,hmat_sess_sorted)
+imagesc(A{1}.xvec, 1:100, hmat_sess_sorted(end-100:end, :))
+% imagesc(A{1}.xvec,1:sum(sessionsByNumSpikes>100),hmat_high_firing)
 % 
-% hmat_high_firing = zeros(sum(sessionsByAvgFR>100), numel(A{1}.xvec));
-% 
-% hmat_high_firing = hmat_sess_sorted(:, indicesAvgFR(sessionsByAvgFR>100));
-% % imagesc(A{1}.xvec,1:usableSessions,hmat_sess_sorted)
-% imagesc(A{1}.xvec,1:sum(sessionsByAvgFR>100),hmat_high_firing)
-% % 
-% % for 1:length(B)
-% %     if sessionsByAvgFR>100:
-% %         hma
-% %     end
-% % end
+% for 1:length(B)
+%     if sessionsByAvgFR>100:
+%         hma
+%     end
+% end
 
-%% z-scored
+%% z-scored PSTH
+% subtract the mean firing rate and normalize by STD
 
-    
+hmat_sess_sorted(end, :);                   % choose highest firing rate session
+plot(A{1}.xvec, hmat_sess_sorted(end, :), 'k','linewidth',2)    % plot original PSTH
 
+avgFR = mean(hmat_sess_sorted(end, :));     % find the session's avg firing rate
+normalized_PSTH = hmat_sess_sorted(end, :) ./ avgFR;    % divide each bin by average firing rate
 
+plot(A{1}.xvec, normalized_PSTH)            % plot z-scored PSTH
+
+%% z-scored heatmap
+avgFR_allsessions = mean(hmat_sess_sorted, 2);  % find mean of hmat_sess_sorted rows
+std_allsessions = std(hmat_sess_sorted, 0, 2);  % find STD of hmat_sess_sorted rows; w = 0 (default)
+normalized_hmat = (hmat_sess_sorted - avgFR_allsessions) ./ std_allsessions;
+
+imagesc(A{1}.xvec, 1:usableSessions, normalized_hmat)
+title('z-scored heatmap of firing rate for each session')
+colormap default
+xlabel('time (s)')
+ylabel('session')
+set(gca,'fontsize',15)
+set(gca, 'TickDir', 'out'); box off; colorbar
