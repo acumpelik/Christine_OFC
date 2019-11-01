6cf2ee8b36c753377cca20446e9b2819ded97b24
acumpelik <acumpelik@gmail.com>
2019-11-01 18:21:42 -0400

    update

---
+++ from_David_et_al/example_plot_4_Andrea.m
@@ -0,0 +1,85 @@
+
+%sampels script to look at psth by reward volume
+
+%make sure to change to where yoru directory of data lives!
+cd('~/projects/ofc/data/PhysiologyData/Christine_OFC_2019')
+[fnames, units, ~, ~] = getfnames;
+f_ind = 741;
+
+load(strcat([fnames{f_ind},'.mat']))
+[~, chosenval, hits, ~] = parse_choices(S);
+
+%% 
+
+%basic way to plot a psth
+%plot(xvec_start,nanmean(hmat_start,1))
+
+%filter by rewards and certain rew. volume
+R6_mask = chosenval==6;
+R12_mask = chosenval==12;
+R24_mask = chosenval==24;
+R48_mask = chosenval==48;
+
+
+figure(1)
+clf
+plot(xvec_start,nanmean(hmat_start,1),'k','linewidth',2)
+hold on
+plot(xvec_start,nanmean(hmat_start(R6_mask,:),1),'linewidth',2)
+
+plot(xvec_start,nanmean(hmat_start(R12_mask,:),1),'linewidth',2)
+
+plot(xvec_start,nanmean(hmat_start(R24_mask,:),1),'linewidth',2)
+
+plot(xvec_start,nanmean(hmat_start(R48_mask,:),1),'linewidth',2)
+
+set(gca,'fontsize',15)
+xlabel('time (s)')
+ylabel('rate (Hz)')
+title('PSTH, single neuron, by reward volume')
+legend('averaged','6 ul','12','24','48')
+
+%% look at all session rewards
+
+%load a concatenated data set that David made, and you have the code for
+a = load('concatdata_ofc_start.mat');
+A = a.A;
+
+%%
+ns_old = numel(A);
+usableVec = zeros(numel(A),1); %mask of sessions that are usable
+for j = 1:ns_old
+    %if you want to do more filtering, add it here. you arleady code for it
+    %to remove multi unit
+    if A{j}.isUsable
+        usableVec(j) = 1;
+    end
+end
+nsess = sum(usableVec); %number of usable session
+
+hmat_sess = zeros(nsess,numel(A{1}.xvec));
+
+%loop over each session, and add to hmat_sess;
+use_inds = find(usableVec);
+nspikes_persession = zeros(nsess,1);
+for j = 1:nsess
+    if  A{use_inds(j)}.isUsable %if session is usuable, grab correct index
+     hmat_sess(j,:) = nanmean( A{use_inds(j)}.hmat,1 );
+    end
+    
+    %average number (over trials) of spikes for each session
+    %can chnage to firing rate if you want. would need to makes ure time
+    %corresponds to when those spikes counts were taken
+    nspikes_persession(j) = nanmean(A{use_inds(j)}.nspikes);
+end
+
+
+%% order the data
+[B,I] = sort(nspikes_persession);
+hmat_sess_sorted = hmat_sess(I,:);
+
+
+surf(A{1}.xvec,1:nsess,hmat_sess_sorted)
+xlabel('time (s)')
+ylabel('session')
+set(gca,'fontsize',15)
---
+++ from_David_et_al/plotHeatmapbyRewVol.m
@@ -0,0 +1,58 @@
+%% plot heatmap of all rewarded session
+importDataset
+
+
+% define a heatmap mask with filtered sessions
+hmat_usable = zeros(numUsableSessions,numel(A{1}.xvec));
+
+% loop over each session, and add PSTH to hmat;
+usableInds = find(usableVec);               % indices of usable sessions
+avgSpikesPerSession = zeros(numUsableSessions,1); % define mask for avg # of spikes per session
+for j = 1:numUsableSessions
+    [~, chosenval, hits, ~] = parse_choices(S);
+    if  A{usableInds(j)}.isUsable & chosenval==6           % if session is usable, grab correct index
+     hmat_usable(j,:) = nanmean( A{usableInds(j)}.hmat,1 );
+    end
+    
+    % average number (over trials) of spikes for each session.
+    % can change to firing rate if you want, would need to make sure time
+    % corresponds to when those spikes counts were taken
+    avgSpikesPerSession(j) = nanmean(A{usableInds(j)}.nspikes);
+end
+
+
+%% order the data
+[sessionsByNumSpikes,indicesAvgNumSpikes] = sort(avgSpikesPerSession);
+hmat_sess_sorted = hmat_usable(indicesAvgNumSpikes,:);
+
+% surf(A{1}.xvec,1:numUsableSessions,hmat_sess_sorted)
+imagesc(A{1}.xvec,1:numUsableSessions,hmat_sess_sorted)
+colormap default
+xlabel('time (s)')
+ylabel('session')
+set(gca,'fontsize',15)
+set(gca, 'TickDir', 'out'); box off; colorbar
+
+%% z-score PSTH
+% subtract the mean firing rate and normalize by std
+
+hmat_sess_sorted(end, :);                   % choose highest firing rate session
+plot(A{1}.xvec, hmat_sess_sorted(end, :), 'k','linewidth',2)    % plot original PSTH
+
+avgFR = mean(hmat_sess_sorted(end, :));     % find the session's avg firing rate
+normalized_PSTH = hmat_sess_sorted(end, :) ./ avgFR;    % divide each bin by average firing rate
+
+plot(A{1}.xvec, normalized_PSTH)            % plot z-scored PSTH
+
+%% z-scored heatmap
+avgFR_allsessions = mean(hmat_sess_sorted, 2);  % find mean of hmat_sess_sorted rows
+std_allsessions = std(hmat_sess_sorted, 0, 2);  % find STD of hmat_sess_sorted rows; w = 0 (default)
+normalized_hmat = (hmat_sess_sorted - avgFR_allsessions) ./ std_allsessions;
+
+imagesc(A{1}.xvec, 1:numUsableSessions, normalized_hmat)
+title('z-scored heatmap of firing rate for each session')
+colormap default
+xlabel('time (s)')
+ylabel('session')
+set(gca,'fontsize',15)
+set(gca, 'TickDir', 'out'); box off; colorbar
